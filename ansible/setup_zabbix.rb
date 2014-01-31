736b7abd8a71834094b1bed72d45ffb5fa270fe1
Daniel Stutzman <dtstutz@gmail.com>
2014-01-31 15:51:12 -0700

    setup_zabbix now sets up multiple hosts, configures zabbix server to be the Zabbix Server template, and sets all hosts to take automatic inventory.

--- ansible/setup_zabbix.rb
+++ ansible/setup_zabbix.rb
@@ -1,12 +1,16 @@
 #!/usr/bin/ruby
 require 'zabbixapi'
 
+# For API docs, see:
+#   https://www.zabbix.com/documentation/2.2/manual/api
+#   http://rubydoc.info/gems/zabbixapi/0.6.4/frames
+
 raise "Arg 1: hostname of zabbix server" if ARGV[0].nil?
-IP = ARGV[0]
+SERVER_HOST = ARGV[0]
 
 PASSWORD_PATH = "#{ENV['HOME']}/.zabbix_admin"
 zbx = ZabbixApi.connect({
-  url:      "http://#{IP}/zabbix/api_jsonrpc.php",
+  url:      "http://#{SERVER_HOST}/zabbix/api_jsonrpc.php",
   user:     'Admin',
   password: File.read(PASSWORD_PATH).strip,
 })
@@ -21,19 +25,40 @@ zabbis_servers = group_id_for['Zabbix servers']
 
 template_id_for = zbx.templates.all
 linux_template = template_id_for['Template OS Linux']
+server_template = template_id_for['Template App Zabbix Server']
 
-host_id = zbx.hosts.create_or_update({
-  host: 'todomvc-practice2.do',
-  interfaces: [{
-      type: 1,
-      main: 1,
-      ip: '107.170.30.54',
-      dns: 'todomvc-practice2.do',
-      port: 10050,
-      useip: 1
-  }],
-  groups: [ groupid: linux_servers ],
+ip_host_pairs = %w[
+  162.243.127.91|todomvc-practice3
+  107.170.30.54|todomvc-practice2
+]
+
+all_host_ids = []
+ip_host_pairs.each do |ip_host_pair|
+  ip, hostname = ip_host_pair.split('|')
+  host_id = zbx.hosts.create_or_update({
+    host: hostname,
+    interfaces: [{
+        type: 1,
+        main: 1,
+        ip: ip,
+        dns: "#{hostname}.do",
+        port: 10050,
+        useip: 1
+    }],
+    groups: [ groupid: linux_servers ],
+  })
+  all_host_ids.push host_id
+end
+
+host_id_for = zbx.hosts.all
+server_host_id = host_id_for[SERVER_HOST.sub(/\.do$/, '')]
+
+zbx.templates.mass_add(hosts_id: all_host_ids, templates_id: [linux_template])
+zbx.templates.mass_add(hosts_id: [server_host_id], templates_id: [server_template])
+zbx.client.api_request({
+  method: 'host.massupdate',
+  params: {
+    hosts: all_host_ids.map { |host_id| { hostid: host_id } },
+    inventory_mode: 1,
+  },
 })
-#host_id_for = zbx.hosts.all
-
-zbx.templates.mass_add(hosts_id: [host_id], templates_id: [linux_template])
