a87643a2baa4f8adbc1671122cd8ca7bc75ad326
Daniel Stutzman <dtstutz@gmail.com>
2014-02-04 12:33:25 -0700

    Add Zabbix template for Ubuntu servers with check for non-installed security updates.

--- ansible/setup_zabbix.rb
+++ ansible/setup_zabbix.rb
@@ -15,6 +15,47 @@ zbx = ZabbixApi.connect({
   password: File.read(PASSWORD_PATH).strip,
 })
 
+ubuntu_group_id = zbx.hostgroups.create_or_update(name: 'Ubuntu servers')
+
+ubuntu_template_id = zbx.templates.create_or_update({
+  host: 'Template OS Ubuntu',
+  groups: [{ groupid: ubuntu_group_id }],
+})
+
+security_app_id = zbx.applications.get_id(name: 'Security') ||
+  zbx.applications.create({
+    name: 'Security',
+    hostid: ubuntu_template_id,
+  })
+
+# warning: create_or_update doesn't work; returns error message:
+#   Item uses host interface from non-parent host.
+# ... which doesn't make sense since we're updating for a template.
+num_updates_security_item_id =
+  zbx.items.get_id(name: '# non-installed updates (security)') ||
+  zbx.items.create({
+    name: '# non-installed updates (security)',
+    description: '# non-installed updates (security)',
+    key_: 'debian_updates[security4]',
+    type: 0, # 0=Zabbix agent
+    hostid: ubuntu_template_id,
+    applications: [security_app_id],
+    value_type: 3, # 3=Numeric (unsigned)
+    delay: 3600,
+  })
+
+unless zbx.triggers.get_id(description:
+    'Latest security package(s) not installed yet on {HOST.NAME}')
+  # note: create_or_update doesn't seem to work
+  zbx.triggers.create({
+    description: 'Latest security package(s) not installed yet on {HOST.NAME}',
+    expression:  '{Template OS Ubuntu:debian_updates[security].last()}>0',
+    priority:    2, # 2=warning
+    status:      0, # 0=enabled, strangely enough
+    type:        0, # 0=don't generate multiple problem events
+  })
+end
+
 mediatype_id_for = zbx.mediatypes.all
 zbx.mediatypes.update({
   mediatypeid: mediatype_id_for['Email'],
@@ -39,18 +80,14 @@ zbx.client.api_request({
   },
 })#p zbx.users.all
 
-exit 1
-
 group_id_for = zbx.hostgroups.all
 linux_servers = group_id_for['Linux servers']
-zabbis_servers = group_id_for['Zabbix servers']
 
 template_id_for = zbx.templates.all
 linux_template = template_id_for['Template OS Linux']
 
 ip_host_pairs = %w[
   162.243.127.91|todomvc-practice3
-  107.170.30.54|todomvc-practice2
 ]
 
 all_host_ids = []
@@ -66,12 +103,17 @@ ip_host_pairs.each do |ip_host_pair|
         port: 10050,
         useip: 1
     }],
-    groups: [ groupid: linux_servers ],
+    groups: [{ groupid: linux_servers }, { groupid: ubuntu_group_id }],
   })
   all_host_ids.push host_id
 end
 
-zbx.templates.mass_add(hosts_id: all_host_ids, templates_id: [linux_template])
+zbx.templates.mass_add({
+  hosts_id: all_host_ids,
+  templates_id: [linux_template, ubuntu_template_id],
+})
+
+# set inventory mode to automatic
 zbx.client.api_request({
   method: 'host.massupdate',
   params: {
