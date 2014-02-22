d18561af8ff60d89b37319e8da08e0a418e5231e
Daniel Stutzman <dtstutz@gmail.com>
2014-02-21 19:04:15 -0700

    Modify config for REMOTE selenium tests so they clean database but don't run a web server on port 3000.

--- spec/helper.rb
+++ spec/helper.rb
@@ -10,6 +10,9 @@ require 'rspec'
 require 'capybara/rspec'
 require 'selenium-webdriver' # just to access Selenium constant
 
+Capybara.javascript_driver = :selenium
+Capybara.default_driver = :selenium
+
 if ENV['REMOTE'] == 'true'
   # See https://saucelabs.com/docs/additional-config
   # See https://saucelabs.com/platforms
@@ -31,28 +34,33 @@ if ENV['REMOTE'] == 'true'
       url: "http://#{selenium_host}:#{selenium_port}/wd/hub",
       desired_capabilities: caps
   end
-else
+
+  # connect to database just for database cleaning
+  require 'sequel'
+  require 'logger'
+  $db = Sequel.connect(ENV.fetch('DATABASE_URL'))
+  $db.logger = Logger.new($stdout)
+
+else # if ENV['REMOTE'] isn't true
   browser = :firefox
   Capybara.register_driver :selenium do |app|
     Capybara::Selenium::Driver.new app, browser: browser
   end
   ENV['BROWSER_URL'] = 'http://localhost:3000'
-end
-Capybara.javascript_driver = :selenium
-Capybara.default_driver = :selenium
 
-require 'sinatra'
-set :run, false # don't automatically start sinatra server
-require './backend'
-Capybara.run_server = true
-Capybara.app_host = 'http://0.0.0.0:3000'
-Capybara.server_port = 3000
-Capybara.app = TodomvcBackend::App.new
-Capybara.server do |app, port| # Run web server from 0.0.0.0 so vms can see it
-  require 'rack/handler/webrick'
-  Rack::Handler::WEBrick.run app,
-    Host: '0.0.0.0',
-    Port: port,
-    #AccessLog: [],
-    Logger: WEBrick::Log::new($stdout)
+  require 'sinatra'
+  set :run, false # don't automatically start sinatra server
+  require './backend'
+  Capybara.run_server = true
+  Capybara.app_host = 'http://0.0.0.0:3000'
+  Capybara.server_port = 3000
+  Capybara.app = TodomvcBackend::App.new
+  Capybara.server do |app, port| # Run web server from 0.0.0.0 so vms can see it
+    require 'rack/handler/webrick'
+    Rack::Handler::WEBrick.run app,
+      Host: '0.0.0.0',
+      Port: port,
+      #AccessLog: [],
+      Logger: WEBrick::Log::new($stdout)
+  end
 end
