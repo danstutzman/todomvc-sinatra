26a008ab4c90fecd7edeef80a141bd87a2a188da
Dan Stutzman <dan@danstutzman.com>
2016-03-17 14:15:13 -0600

    Move files to todomvc-backend-go repo.

--- go/run_backend.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash -ex
-go run backend.go handlers.go \
-  -postgres_credentials_path postgres_credentials.dev.json \
-  "$@"
--- go/test_backend.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash -ex
-echo '{"DeviceUid": "test"}' | curl -d @- http://localhost:3000/
